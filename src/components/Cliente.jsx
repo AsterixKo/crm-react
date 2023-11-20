a08d5fa0337592516296c7a77a6589e0fdd5932c
Javier Moneo <jvr.moneo@gmail.com>
2023-11-20 18:33:33 +0100

    Iterando sobre clientes y mostrando la información

---
+++ src/components/Cliente.jsx
@@ -0,0 +1,8 @@
+export const Cliente = ({ cliente }) => {
+  const { nombre, empresa, email, telefono, id } = cliente;
+  return (
+    <tr>
+      <td className="p-6">{nombre}</td>
+    </tr>
+  );
+};
