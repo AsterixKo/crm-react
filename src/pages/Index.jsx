48da8c2e3c06dfd2b6d90313b2ef0faaf5d01aea
Javier Moneo <jvr.moneo@gmail.com>
2023-11-20 18:07:18 +0100

    Creando un loader

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,3 +1,12 @@
+export function loader() {
+  return "Desde Loader";
+}
+
 export const Index = () => {
-  return <div>Clientes</div>;
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+      <p className="mt-3">Administra tus clientes</p>
+    </>
+  );
 };
