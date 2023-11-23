49a222f2bced05973232e24fef044240aff32f09
Javier Moneo <jvr.moneo@gmail.com>
2023-11-23 18:39:12 +0100

    Añadir nuevos clientes en nuestro formulario y JSON-server

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,6 +1,7 @@
-import { useNavigate, Form, useActionData } from "react-router-dom";
+import { useNavigate, Form, useActionData, redirect } from "react-router-dom";
 import { Formulario } from "../components/Formulario";
 import { Error } from "../components/Error";
+import { agregarCliente } from "../data/clientes";
 
 export async function action({ request }) {
   const formData = await request.formData();
@@ -30,7 +31,8 @@ export async function action({ request }) {
     return errores;
   }
 
-  //   return null;
+  await agregarCliente(datos);
+  return redirect("/");
 }
 
 export const NuevoCliente = () => {
